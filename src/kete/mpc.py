14e9c33b849fa19fdf761943908cda87f0035da0
Donovan Schlekat <dschlekat@unc.edu>
2026-05-29 13:09:34 -0400

    MPC extended files use TT not UTC time scales
    * Fixed parsing of MPC files to use TT not UTC time.
    
    * Updated leap second file to reflect IERS Bulletin 71, Jan 2026 - No user facing effect.

--- src/kete/mpc.py
+++ src/kete/mpc.py
@@ -156,8 +156,8 @@ def fetch_known_orbit_data(url=None, force_download=False):
             incl=obj["i"],
             lon_node=obj["Node"],
             peri_arg=obj["Peri"],
-            peri_time=Time(obj["Tp"], scaling="utc").jd,
-            epoch=Time(obj["Epoch"], scaling="utc").jd,
+            peri_time=Time(obj["Tp"], scaling="tt").jd,
+            epoch=Time(obj["Epoch"], scaling="tt").jd,
             arc_len=arc_len,
             name=obj.get("Name", None),
         )
@@ -195,8 +195,10 @@ def fetch_known_comet_orbit_data(force_download=False):
             incl=comet["i"],
             lon_node=comet["Node"],
             peri_arg=comet["Peri"],
-            peri_time=Time.from_ymd(*peri_time).jd,
-            epoch=Time.from_ymd(*epoch_time).jd,
+            # MPC comet files store YMD as TT time.
+            # Support for this will not be added into kete, so here is a workaround.
+            peri_time=Time(Time.from_ymd(*peri_time).utc_jd, scaling="tt").jd,
+            epoch=Time(Time.from_ymd(*epoch_time).utc_jd, scaling="tt").jd,
         )
         objects.append(obj)
     return pd.DataFrame.from_records(objects)
